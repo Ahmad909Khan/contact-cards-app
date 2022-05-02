fbc6d89ce2f209432aba79e1a10e22619419fd06
Ahmad909Khan <you@example.com>
2022-05-02 11:04:25 +0530

    /

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
         )}
         {isLoggedIn && (
           <Route
-            path="home-page"
+            path="/home-page"
             element={<HomePage loading={loading} />}>
             <Route index element={<AllCardsComponent />} />
             <Route path='all-cards' element={<AllCardsComponent />} />
